4afcd069f6a69a895d118af486cf00d1274558e4
xenia <ksen.novikova@mail.ru>
2022-10-24 20:59:40 +0300

    дай бог

---
+++ src/Author.java
@@ -0,0 +1,24 @@
+public class Author {
+    String name;
+    String surname;
+    public  Author(String name, String surname){
+        this.name=name;
+        this.surname=surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
